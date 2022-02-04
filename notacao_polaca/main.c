766a8617de925937df17d46a2c04e14675b3e927
Yevheniy <yevheniykushnirenko@gmail.com>
2022-02-04 14:03:14 +0000

    teste de branch

--- notacao_polaca/main.c
+++ notacao_polaca/main.c
@@ -13,12 +13,13 @@
 typedef struct no_lista {
     //que tipo de dados uso para guardar elementos de tamanho variavel na lista?
     //flexible array member??
-    char valor[50];
+    char operador;
+    float numero;
     struct no_lista *proximo;
 }no;
 
 
-no *criar_novo_no(no *valor ) {
+no *criar_novo_no(no * ) {
     no *resultado;
     resultado = malloc(sizeof(no));
     strcpy(resultado->valor,valor);
@@ -68,107 +69,6 @@ int main () {
     char *eq_polaca=NULL; //Ler input de utilizador com scanf com o modificador 'm' para alocar a memoria dinamicamente
     char* token;
 
-#if 0
-//Varios testes para entender progressivamento o funcionamento da lista e ganhar um maior nivel de controlo
-
-
-//    n1=malloc(sizeof(no));
-//    n2=malloc(sizeof(no));
-//    n3=malloc(sizeof(no));
-
-
-//    printf("Input: ");
-//    scanf(" %c", &in);
-//    n1->valor = in;
-//    printf("Input: ");
-//    scanf(" %c", &in);
-//    n2->valor = in;
-//    printf("Input: ");
-//    scanf(" %c", &in);
-//    n3->valor = in;
-//
-//
-//    inicio = n2;
-//    n2->proximo = n1;
-//    n1->proximo = n3;
-//    n3->proximo = NULL;
-
-//    printf("Input: ");
-//    scanf(" %c", &in);
-//    n1=criar_novo_no(in);
-//    printf("Input: ");
-//    scanf(" %c", &in);
-//    n2=criar_novo_no(in);
-//    printf("Input: ");
-//    scanf(" %c", &in);
-//    n3=criar_novo_no(in);
-
-
-//    for(int i = 0; i<3; i++){
-//        printf("Insira um carcter: ");
-//        scanf(" %c", &in);
-//        temp = criar_novo_no(in);
-//        temp->proximo = inicio;
-//        inicio = temp;
-//    }
-
-
-//    temp = criar_novo_no('a');
-//    inicio=temp;
-//    temp=criar_novo_no('z');
-//    temp->proximo=inicio;
-//    inicio=temp;
-//    temp=criar_novo_no('f');
-//    temp->proximo=inicio;
-//    inicio=temp;
-
-//    printf("Input: ");
-//    scanf(" %c", &in);
-//    temp = criar_novo_no(in);
-//    inicio=temp;
-//    printf("Input: ");
-//    scanf(" %c", &in);
-//    temp=criar_novo_no(in);
-//    temp->proximo=inicio;
-//    inicio=temp;
-//    printf("Input: ");
-//    scanf(" %c", &in);
-//    temp=criar_novo_no(in);
-//    temp->proximo=inicio;
-//    inicio=temp;
-
-//    for(int i=0;i<3;i++){
-//        printf("Input: ");
-//        scanf(" %c", &in);
-//        temp=criar_novo_no(in);
-//        inserir_inicio(&inicio,temp);
-//    }
-//
-//    imprimir_lista(inicio);
-//
-//    printf("\n\n\n");
-
-    //inserir_depois_de_no(temp, criar_novo_no(in));
-
-//    for(int i=0;i<3;i++){
-//        printf("Input: ");
-//        scanf(" %c", &in);
-//        inserir_depois_de_no(temp, criar_novo_no(in));
-//    }
-
-    //temp= pesquisa(inicio,'+');
-
-    //printf("\n\n Encontri o node com %c", temp->valor);
-
-//    do{
-//
-//        printf("Input: ");
-//        scanf(" %c", &in);
-//        temp=criar_novo_no(in);
-//        inserir_inicio(&inicio,temp);
-//
-//    }while(in!='!');
-#endif // 0
 
 //
 //    char* ptr = NULL;
