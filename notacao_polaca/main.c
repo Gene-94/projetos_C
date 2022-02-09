0ba5a83cd6f3dc1bdce479cdf672ca39cc9d6317
Yevheniy <yevheniykushnirenko@gmail.com>
2022-02-09 15:18:06 +0000

    codigo de nota_polaca.c copiado para main.c, debuges do VS não estava a funcionar...

--- notacao_polaca/main.c
+++ notacao_polaca/main.c
@@ -1,111 +1,97 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#define OPERADORES "+-:/x*"
 
-//#define SPC ' '
- //p->a == (*p).a
+//struct para inserir tanto valores como operadores
 
- //a criar uma lista para ler toda a equação de uma vez ate encontrar caracter de escape
- //apos ler a equação percorer a equação da direita para a esquerda ate encontrar um operador
- //apos encontrar um operador, identificar o tipo de operação a aplicar e aplicar a operação nos dois nos mais a direita da equação, que serão necessariamente numeros
- //depois de realizar a operação substituir o valor do no do operador pelo resultado da operação e eleminar os dois nos mais a direita, assim simplicando fazeadamente a equação da direita para esquerda, tendo em conta as prioridades de operação
- //poderiamos fazer com um array, mas haveria situações em que os numeros envolvidos na operação não seriam os que estão mais a direita, necessitando assim de remover os operandos depois de cada opeação, para poder manter a regra e continuar a dar sweep da direita para a esquerda
-typedef struct no_lista {
-    //que tipo de dados uso para guardar elementos de tamanho variavel na lista?
-    //flexible array member??
-    char operador;
+// typedef struct eq {
+//     int nr;
+//     char opr;
+//     struct eq *proximo;
+// } eq;
+
+
+
+//duas structs, uma contendo os numeros e outra os operadores
+//desta forma quando alocar a memoria não reserva floats e char quando não é necessario, fa-lo separadamente
+
+typedef struct nr {
     float numero;
-    struct no_lista *proximo;
-}no;
+    struct nr *proximo;
+}nr;
+
+typedef struct opr{
+    char operador;
+    struct opr *proximo;
+}opr;
+
+void pedir(char *string);
+int validar(char *string);
+
+int main (){
+    char *token;
+    //char *str_eq;
+    char str_eq[150] = {0};
+
+    printf("\t***** Calculadora Polaca *****\n");
+
+    //o str_eq vai servir como uma especie de buffer, depois de ser carregado para a lista podemos libertar esta memoria
+    //str_eq = (char *)malloc(150*sizeof(char));
+    //não consigo passar os ponteiros corretamente se usar malloc/calloc
+
+    do{
+        // pedir expressão + instruções
+        pedir(str_eq);
+
+        //validar o input
+        validar(str_eq);
+    }while(validar(str_eq));//se não for validado volta a pedir
 
 
-no *criar_novo_no(no  ) {
-    no *resultado;
-    resultado = malloc(sizeof(no));
-    strcpy(resultado->valor,valor);
-    //resultado->valor=valor;
-    resultado->proximo=NULL;
-    return resultado;
-}
-
-//de momento esta a ser usada para criar pilha, de forma a dar sweep no input começando pelo ultimo intoduzido, que será um caracter de escape
-no *inserir_inicio(no **inicio, no *no_x){
-    no_x->proximo=*inicio;
-    *inicio = no_x;
-    return no_x;
-}
-
-
-//de momento sem uso, pode ser usada para inverter a lista e permitir escolher entre polaca e polaca inversa
-void inserir_depois_de_no(no *no_anterior, no *novo_no){
-    novo_no->proximo = no_anterior->proximo;
-    no_anterior->proximo=novo_no;
-}
-
-no *pesquisa (no *inicio, no *valor) {
-    no *tpm = inicio;
-    while (tpm != NULL){
-        if (tpm->valor==valor) return tpm;
-        tpm=tpm->proximo;
+    //separar a equação nas suas partes individuais
+    //confirmar se é numero ou operador
+    //carregar para a lista
+    token = strtok(str_eq," ");
+    while (token != NULL){
+        token = strtok(NULL," ");
     }
-    return NULL;
-}
-
-void imprimir_lista(no *inicio) {
-    no *temporario;
-    temporario = inicio;
-
-    while (temporario != NULL) {
-        printf("%s", temporario->valor);
-        temporario = temporario->proximo;
-    }
-}
-
-int main () {
-    no *temp;
-    //no *n1,*n2,*n3;
-    no *inicio=NULL;
-    //char in; // em vez de ler carater a carater, linha a linha quero que o utilizador possa intoduzir a equação, do tamanho que desejar toda numa linha
-    char *eq_polaca=NULL; //Ler input de utilizador com scanf com o modificador 'm' para alocar a memoria dinamicamente
-    char* token;
-
-
-//
-//    char* ptr = NULL;
-//    scanf("%m[^0]", &ptr );
-//    printf("%s %d\n", ptr, strlen(ptr));
-//    for(int i=0; i<strlen(ptr);i++){
-//        printf("\nnumero do elemento no array: %d | conteudo: %c",i,ptr[i]);
-//    }
-//    token = strtok(ptr, " ");
-//    printf("\n%s", token);
-//    getchar();
-//    getchar();
-
-
-    printf("Insira a equação em notação polaca: ");
-    scanf("%m[^\n]", &eq_polaca );//le linha toda e aloca memoria dinamicamente
-
-
-    //carregar o array eq_polaca[] para a lista
-
-    token=strtok(eq_polaca," ");
-
-    while(token!=NULL){
-        printf("%s\n", token);
-        temp=criar_novo_no(token);
-        inserir_inicio(&inicio,temp);
-        token=strtok(NULL," ");
-    }
-
-
-    free(eq_polaca);//depois de carregado para a lista já podemos libertar a memoria
-
-
-    imprimir_lista(inicio);//vai imprimir na ordem inversa a introduzida
 
 
 
     return 0;
 }
 
+void pedir(char *string){
+
+    // pedir expressão + instruções
+    printf("\nInsira a sua equação na forma de notação polaca,com espaço entre os numero e operadores, abaixo.\n");
+    printf("(exemplo: + * - + 2 3 5 33 + 3 4)");
+    printf("\nEquação: ");
+
+    //ler a equação escrita pelo utilizador toda numa linha so
+    scanf("%149[^\n]", string);
+}
+
+int validar(char *string){
+    //validar o input
+    /* se diferente de [0-9] e de OPERADORES e de " " */
+    char c_permit[] ="+-x*/:0123456789 ";
+
+    for(int i=0;string[i]!=0;i++) {
+        int k=0;
+        for(int j=0; c_permit[j]!=0;j++){
+
+            if(c_permit[j]==string[i]){
+                k=1;
+                break;
+            }
+        }
+        if(k==0){
+            printf("\nForam identificados caracteres não reconhecidos na sua expressão.\n");
+            printf("Por favor volte a inserir a sua equação, conferindo de que segue as indicações.\n");
+            return 1;
+        }
+    }
+    return 0;
+}
