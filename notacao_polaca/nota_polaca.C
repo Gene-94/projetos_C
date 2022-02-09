5ed40b09ff2580de9cd10af61a0ce63b48bcd8fa
Yevheniy <yevheniykushnirenko@gmail.com>
2022-02-09 15:00:28 +0000

    Criadas funçoes pedir e validar, str_eq passou a ser alocada estaticamente

--- notacao_polaca/nota_polaca.C
+++ notacao_polaca/nota_polaca.C
@@ -1,39 +1,95 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#define OPERADORES "+-:/x*"
 
-typedef struct eq {
-    int nr;
-    char opr;
-    struct eq *proximo;
-} eq;
+//struct para inserir tanto valores como operadores
+
+// typedef struct eq {
+//     int nr;
+//     char opr;
+//     struct eq *proximo;
+// } eq;
+
+
+
+//duas structs, uma contendo os numeros e outra os operadores
+//desta forma quando alocar a memoria não reserva floats e char quando não é necessario, fa-lo separadamente 
+
+typedef struct nr {
+    float numero;
+    struct nr *proximo;
+}nr;
+
+typedef struct opr{
+    char operador;
+    struct opr *proximo;
+}opr;
+
+void pedir(char *string){
+    
+    // pedir expressão + instruções
+    printf("\nInsira a sua equação na forma de notação polaca,com espaço entre os numero e operadores, abaixo.\n");
+    printf("(exemplo: + * - + 2 3 5 33 + 3 4)");
+    printf("\nEquação: ");
+    
+    //ler a equação escrita pelo utilizador toda numa linha so
+    scanf("%149[^\n]", string);
+}
+
+int validar(char *string){
+    //validar o input 
+    /* se diferente de [0-9] e de OPERADORES e de " " */ 
+    char c_permit[] ="+-x*/:0123456789 ";
+
+    for(int i=0;string[i]!=0;i++) {
+        int k=0;
+        for(int j=0; c_permit[j]!=0;j++){
+
+            if(c_permit[j]==string[i]){
+                k=1;
+                break;
+            }
+        }
+        if(k==0){
+            printf("\nForam identificados caracteres não reconhecidos na sua expressão.\n");
+            printf("Por favor volte a inserir a sua equação, conferindo de que segue as indicações.\n");
+            return 1;
+        }   
+    }
+    return 0;
+}
+
 
 
 int main (){
-    char *str_eq, *token;
-    
-    char operadores[] ="+-x*/:";
+    char *token;
+    //char *str_eq;
+    char str_eq[150] = {0};
 
     printf("\t***** Calculadora Polaca *****\n");
-    printf("\nInsira a sua equação na forma de notação polaca,com espaço entre os numero e operadores, abaixo.\n");
-    printf("(exemplo: + * - + 2 3 5 33 + 3 4");
-    printf("Esquação: ");
 
-    str_eq = (char *)calloc(150,sizeof(char));
-    scanf("%149[^\n]", str_eq);
+    //o str_eq vai servir como uma especie de buffer, depois de ser carregado para a lista podemos libertar esta memoria
+    //str_eq = (char *)malloc(150*sizeof(char));
+    //não consigo passar os ponteiros corretamente se usar malloc/calloc
 
-    for(int i=0; str_eq[i]!=0;i++){
-        if()
-    }
+    do{
+    // pedir expressão + instruções
+    pedir(str_eq);
 
-    eq *fim = malloc(sizeof(int));
-    token = strtok(str_eq, " ");
-    //strtof(token, NULL, 10);
-    fim->nr = atof(token);
+    //validar o input 
+    validar(str_eq); 
+    }while(validar(str_eq));//se não for validado volta a pedir
+
+
+    //separar a equação nas suas partes individuais
+    //confirmar se é numero ou operador
+    //carregar para a lista
+    token = strtok(str_eq," ");
     while (token != NULL){
-        token = strtok(NULL, " ");
+        token = strtok(NULL," ");
     }
-    
+
 
 
     return 0;
